62c3a1a8f937f9f542cb0bdaaf2082ff0b786c18
RandallKeur7 <108151926+RandallKeur7@users.noreply.github.com>
2023-11-19 18:34:34 -0500

    made additional changes to get liquibase running at startup hopefully

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import dotenv, {config} from 'dotenv';
+import {Liquibase} from 'liquibase';
 dotenv.config();
 
 const pool = mysql.createPool({
@@ -8,5 +9,37 @@ const pool = mysql.createPool({
     password: process.env.MYSQL_PASSWORD,
     database: process.env.MYSQL_DATABASE
 }).promise()
+/*
+// Having issues with connection here
+const conn = mysql.createConnection({
+    url: 'jdbc:mysql://localhost:3306/pharmacy?createDatabaseIfNotExist=true',
+    host: 'localhost',
+    username: 'root',
+    password: 'root',
+    database: 'pharmacy'
+})
 
- 
+conn.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected to MySql DB!");
+})
+*/
+const myConfig = {
+    driver: 'com.mysql.cj.jdbc.Driver',
+    classpath: 'liquibase/mysql-connector-j-8.2.0.jar',
+    changeLogFile: 'liquibase/master-change-log.yaml',
+    url: 'jdbc:mysql://localhost:3306/pharmacy?createDatabaseIfNotExist=true',
+    username: 'root',
+    password: 'root'
+}
+
+const liquibase = new Liquibase(myConfig);
+
+try {
+    console.log("Running Liquibase changes")
+    await liquibase.dropAll();
+    await liquibase.update();
+}
+catch (err) {
+    console.log("Liquibase startup failed, See error message: " + err.message);
+}
