3353b8b3352ddfd940edfc2bbc4c614eb5e4b98e
RandallKeur7 <108151926+RandallKeur7@users.noreply.github.com>
2023-11-20 19:55:38 -0500

    add make file command and stopping point for local db setup working with database.js

--- database.js
+++ database.js
@@ -1,44 +1,38 @@
 import mysql from 'mysql2';
-import dotenv, {config} from 'dotenv';
+import dotenv from 'dotenv';
 import {Liquibase} from 'liquibase';
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USERNAME,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise()
-/*
-// Having issues with connection here
 const conn = mysql.createConnection({
-    url: 'jdbc:mysql://localhost:3306/pharmacy?createDatabaseIfNotExist=true',
-    host: 'localhost',
-    username: 'root',
-    password: 'root',
-    database: 'pharmacy'
+    url: process.env.MYSQL_URL,
+    host: process.env.MYSQL_HOST,
+    database: process.env.MYSQL_DATABASE,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
 })
 
 conn.connect(function (err) {
     if (err) throw err;
-    console.log("Connected to MySql DB!");
+    console.log("Connected to MySql DB");
 })
-*/
+
 const myConfig = {
-    driver: 'com.mysql.cj.jdbc.Driver',
+    driver: process.env.MYSQL_DRIVER,
     classpath: 'liquibase/mysql-connector-j-8.2.0.jar',
     changeLogFile: 'liquibase/master-change-log.yaml',
-    url: 'jdbc:mysql://localhost:3306/pharmacy?createDatabaseIfNotExist=true',
-    username: 'root',
-    password: 'root'
+    url: process.env.MYSQL_URL,
+    username: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD
 }
 
 const liquibase = new Liquibase(myConfig);
 
 try {
-    console.log("Running Liquibase changes")
+    console.log("Dropping database to re-initialize")
     await liquibase.dropAll();
+    console.log("Running Liquibase changes")
     await liquibase.update();
+    console.log("Database ready to use")
 }
 catch (err) {
     console.log("Liquibase startup failed, See error message: " + err.message);
--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-db-up:
+db-docker:
 	make db-down
 	docker compose -f Dockerfile-mysql.yaml up -d
 	sleep 10
@@ -9,3 +9,6 @@ db-down:
 
 liquibase-update:
 	node-liquibase update --changeLogFile="liquibase/master-change-log.yaml" --url="jdbc:mysql://localhost:3307/pharmacy?createDatabaseIfNotExist=true" --username="root" --password="root" --classPath="liquibase/mysql-connector-j-8.2.0.jar" --driver="com.mysql.cj.jdbc.Driver"
+
+db-local:
+	node database.js
