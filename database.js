e1bc90a4f0dd97e496e5ccdd54cc4c3150c7b8a3
RandallKeur7 <108151926+RandallKeur7@users.noreply.github.com>
2023-11-21 22:30:03 -0500

    create get endpoints and spin up database with liquibase running at startup, stopping points, need to create repository class to abstract logic and queries to prevent duplication of code

--- database.js
+++ database.js
@@ -3,17 +3,11 @@ import dotenv from 'dotenv';
 import {Liquibase} from 'liquibase';
 dotenv.config();
 
-const conn = mysql.createConnection({
-    url: process.env.MYSQL_URL,
+export const conn = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     database: process.env.MYSQL_DATABASE,
     user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD
-})
-
-conn.connect(function (err) {
-    if (err) throw err;
-    console.log("Connected to MySql DB");
 })
 
 const myConfig = {
@@ -25,15 +19,17 @@ const myConfig = {
     password: process.env.MYSQL_PASSWORD
 }
 
-const liquibase = new Liquibase(myConfig);
+export const liquibase = new Liquibase(myConfig);
 
-try {
-    console.log("Dropping database to re-initialize")
-    await liquibase.dropAll();
-    console.log("Running Liquibase changes")
-    await liquibase.update();
-    console.log("Database ready to use")
-}
-catch (err) {
-    console.log("Liquibase startup failed, See error message: " + err.message);
+export async function initLiquibase() {
+    try {
+        console.log("Dropping database to re-initialize")
+        await liquibase.dropAll();
+        console.log("Running Liquibase changes")
+        await liquibase.update();
+        console.log("Database ready to use")
+    }
+    catch (err) {
+        console.log("Liquibase startup failed, See error message: " + err.message);
+    }
 }
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-// const express = require('express')
 import express from "express";
+import bodyParser from "body-parser";
+import {conn, initLiquibase} from "./database.js"
 const app = express()
 app.set("view engine, ejs")
+app.use(bodyParser.json());
 const port = 8080
+
+initLiquibase().then(r => conn.connect(function (err) {
+  if (err) throw err;
+  console.log("Connected to MySql DB");
+}));
 
 app.get('/', (req, res) => {
   res.render("index.ejs")
@@ -15,4 +22,59 @@ app.get('/signin', (req, res) => {
 app.use(express.static("public"));
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+})
+
+app.get('/doctors', (req, res) => {
+  conn.query('SELECT * FROM Doctor', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err)
+  })
+});
+
+app.get('/patients', (req, res) => {
+conn.query('SELECT * FROM Patient', (err, rows, fields) => {
+  if (!err)
+    res.send(rows);
+  else
+    console.log(err)
+})
+});
+
+
+app.get('/insurance', (req, res) => {
+  conn.query('SELECT * FROM Insurance', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err);
+  })
+})
+
+app.get('/patient/doctors', (req, res) => {
+  conn.query('SELECT * FROM PatientDoctor', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err);
+  })
+})
+
+app.get('/prescriptions', (req, res) => {
+  conn.query('SELECT * FROM Prescription', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err);
+  })
+})
+
+app.get('/patient/prescriptions', (req, res) => {
+  conn.query('SELECT * FROM PatientPrescription', (err, rows, fields) => {
+    if (!err)
+      res.send(rows);
+    else
+      console.log(err);
+  })
 })
