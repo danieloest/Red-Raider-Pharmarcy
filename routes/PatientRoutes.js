7a741c0670fb185ad2bd197d4f4099f45779bfe0
Daniel Oestreicher <Daniel.Oestreicher@ibm.com>
2023-11-28 19:23:35 -0600

    create patient routes

---
+++ routes/PatientRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const PatientController = require("../controllers/PatientController.js");
+const PatientPermission = require("../middleware/PatientPermission.js");
+const router = express.Router();
+
+router.get("/:patientId", PatientController.getPatient);
+
+router.get("/", PatientController.getAllPatients);
+
+router.patch("/", PatientController.updatePatient);
+
+router.delete("/:patientId", PatientController.deletePatient);
+
+module.exports = router;
